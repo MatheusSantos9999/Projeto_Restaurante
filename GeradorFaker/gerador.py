120741b8fe39c865e1952cb2f8b85e0d43f701b1
MatheusAlves <adeusbolacha@gmail.com>
2026-09-16 00:35:12 -0300

    feat: criação em estágio inicial do gerador de clientes ficticios

--- GeradorFaker/gerador.py
+++ GeradorFaker/gerador.py
@@ -4,28 +4,7 @@ from faker import Faker
 def gerador_cliente(numero):
 
     fk = Faker(locale='pt-BR')
-    """
-    cardapio_salgados = {
-        'Coxinha de Frango': 8.00,
-        'Pastel de Carne': 10.00,
-        'Pastel de Calabresa': 12.00,
-        'Pastel 3 queijos': 10.00,
-        'Empada de Frango': 8.00,
-        'Empada de Palmito': 9.00,
-        'Kibe': 7.50,
-        'Pão de Queijo': 3.00,
-        'Bolinho de Bacalhau': 8.00
-    }
-
-    cardapio_bebidas = {
-        'Coca-Cola 600ml': 6.00,
-        'Coca-Cola 350ml': 5.00,
-        'Caldo de Cana 2L': 12.00,
-        'Coca-Cola 2L': 14.50,
-        'Pepsi 2L': 13.50
-                        }
-    """
-
+  
     lista_cliente = []
 
     num_comanda = 0
